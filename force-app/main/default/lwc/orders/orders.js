55ab843a2af975ca654587da25718961a0e70002
Naila Hamida <naila.hamida@gmail.com>
2025-05-15 00:04:53 -0400

    finale version

--- force-app/main/default/lwc/orders/orders.js
+++ force-app/main/default/lwc/orders/orders.js
@@ -1,5 +1,5 @@
 import { LightningElement, api } from 'lwc';
-import getSumOrdersByAccount from '@salesforce/apex/MyTeamOrdersController.getSumOrdersByAccount';
+import getSumOrdersByAccount from '@salesforce/apex/OrdersController.getSumOrdersByAccount';
 
 export default class Orders extends LightningElement {
     @api recordId;
